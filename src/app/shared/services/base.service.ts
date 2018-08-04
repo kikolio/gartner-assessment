eb28b8c8c480f829b41b9ef00b90e1070354f3f8
Erik Zambrano <erik@austincodingacademy.com>
2018-08-04 13:16:14 -0500

    Category service and model

---
+++ src/app/shared/services/base.service.ts
@@ -0,0 +1,10 @@
+import { HttpHeaders } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
+
+export class BaseService {
+    protected httpOptions = {
+        headers: new HttpHeaders( { 'Content-Type' : 'application/json'})
+    };
+
+    protected api_url = environment.api_url;
+}
---
+++ src/app/shared/services/category.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Category } from "./category";
+import { BaseService } from "./base.service";
+
+@Injectable()
+export class CategoryService extends BaseService {
+    constructor(private http: HttpClient) {
+        super();
+     }
+
+    public getCategories(): Observable<Category[]> {
+        return this.http.get<Category[]>(`${this.api_url}/Categories`);
+    }
+}
